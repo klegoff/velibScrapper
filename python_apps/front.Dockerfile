128c4a3a55a51cc0404595644fbd6156311afd0a
klegoff <killian.legoff@free.fr>
2024-09-07 16:27:19 +0000

    Write front app, add requirements and port exposure

--- python_apps/front.Dockerfile
+++ python_apps/front.Dockerfile
@@ -5,10 +5,15 @@ FROM python:3.9.13
 WORKDIR /app
 
 COPY requirements.txt ./
+COPY requirements_front.txt ./
 COPY front.py ./
 COPY connection_utils.py ./
 
 RUN pip3 install -r requirements.txt
+RUN pip3 install -r requirements_front.txt
 
-ENTRYPOINT ["python", "front.py"]
+EXPOSE 8050
+
+CMD gunicorn --bind 0.0.0.0:8050 front:server
+#ENTRYPOINT ["python", "front.py"]
 #ENTRYPOINT ["tail", "-f", "/dev/null"]
--- python_apps/front.py
+++ python_apps/front.py
@@ -1,2 +1,45 @@
-import logging
-logging.warning("hello world")
+import os
+from connection_utils import connectDB, retrieveData
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output,callback
+
+# DB connection
+USER=os.getenv("POSTGRES_USER")
+PASSWORD=os.getenv("POSTGRES_PASSWORD")
+HOST="db_app"
+PORT=5432
+DATABASE=os.getenv("POSTGRES_DB")
+
+engine = connectDB(username=USER, host=HOST, password=PASSWORD, database=DATABASE)
+
+# get data (need refresh ?)
+station = retrieveData(engine, table="station").sort_values("name").set_index("stationcode")
+hist = retrieveData(engine, table="historic")
+name_to_code = {v:k for k,v in station["name"].to_dict().items()}
+
+app = Dash(__name__)
+
+server = app.server
+
+app.layout = html.Div([
+    dcc.Dropdown(station["name"].tolist(), '11 Novembre 1918 - 8 Mai 1945', id='station_name'),
+    dcc.Graph(id='graph')
+        ]
+    )
+
+@callback(
+    Output('graph', 'figure'),
+    Input('station_name', 'value'),
+)
+def update_figure(selected_station_name):
+    stationcode = name_to_code[selected_station_name]
+    data = hist.loc[hist.stationcode == stationcode]
+    data = data.groupby("duedate").min().reset_index() # duplicate management
+    if data.shape[0] > 0:
+        fig = px.line(data,x="duedate", y=["ebike","mechanical", "numbikesavailable"],markers="o")
+        return fig
+    else:
+        return None
+        
+if __name__ == "__main__":
+    app.run(debug=True)
